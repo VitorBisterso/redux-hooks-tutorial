503248a4dcd9a8463b06640be6e31072d2be5bf6
Vitor Bisterso dos Santos <vitorbisterso@gmail.com>
2021-03-02 13:44:37 -0300

    Add redux hooks

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { actions } from '../actions/counter';
+import { selectors } from '../selectors/counter';
 
-const Counter = ({ counter, decrement, increment }) => (
-  <>
-    <h1>Counter: {counter}</h1>
-    <ul>
-      <li><button onClick={decrement}>Decrement</button></li>
-      <li><button onClick={increment}>Increment</button></li>
-    </ul>
-  </>
-);
+const Counter = () => {
+  const counter = useSelector(selectors.getCounter);
+  const dispatch = useDispatch();
+  
+  const handleDecrement = () => dispatch(actions.decrement());
+  const handleIncrement = () => dispatch(actions.increment());
+  return (
+    <>
+      <h1>Counter: {counter}</h1>
+      <ul>
+        <li><button onClick={handleDecrement}>Decrement</button></li>
+        <li><button onClick={handleIncrement}>Increment</button></li>
+      </ul>
+    </>
+  )
+};
 
-const mapStateToProps = state => ({
-  counter: state.counterReducers.counter
-});
-
-const mapDispatchToProps = dispatch => ({
-  decrement: () => dispatch(actions.decrement()),
-  increment: () => dispatch(actions.increment())
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Counter);
+export default Counter;
