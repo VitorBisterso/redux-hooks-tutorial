fcfb06acfc8562a8097b20bcecb0389b6ae2c8bb
Vitor Bisterso dos Santos <vitorbisterso@gmail.com>
2021-03-02 13:27:53 -0300

    Add basic and well known redux config

---
+++ src/actions/counter.js
@@ -0,0 +1,10 @@
+const actions = {
+  decrement: () => ({
+    type: 'COUNTER_DECREMENT'
+  }),
+  increment: () => ({
+    type: 'COUNTER_INCREMENT'
+  })
+};
+
+export { actions };
---
+++ src/components/Counter.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import { actions } from '../actions/counter';
+
+const Counter = ({ counter, decrement, increment }) => (
+  <>
+    <h1>Counter: {counter}</h1>
+    <ul>
+      <li><button onClick={decrement}>Decrement</button></li>
+      <li><button onClick={increment}>Increment</button></li>
+    </ul>
+  </>
+);
+
+const mapStateToProps = state => ({
+  counter: state.counterReducers.counter
+});
+
+const mapDispatchToProps = dispatch => ({
+  decrement: () => dispatch(actions.decrement()),
+  increment: () => dispatch(actions.increment())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
